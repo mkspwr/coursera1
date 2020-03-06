5cedc41eb587fca29e8a2706a5745061ae9e3fbd
Kakkoo <58194538+Kakkoo@users.noreply.github.com>
2020-03-05 21:00:02 -0800

    test

---
+++ js/scriptLearn1.js
@@ -0,0 +1,33 @@
+function sayHello () {
+    this.textContent = "Said it!";
+        var name=
+        document.getElementById("name").value;
+        var message = "<h2>Hello  " + name +"!<h2>";
+
+        //document
+         //.getElementById("content")
+        //.textContent = message;
+
+        document
+        .getElementById("content")
+        .innerHTML = message;
+
+        if (name === "student") {
+            var title = 
+            document
+            .querySelector("#title")
+            .textContent;
+            title += "& Lovin' it!";
+            document
+            .querySelector("#title")
+            .textContent = title;
+
+           
+        }
+    
+}
+document.querySelector("button")
+.addEventListener("click", sayHello);
+
+//document.querySelector("button")
+//.onclick = sayHello;
